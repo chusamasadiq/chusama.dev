85a790c33912959b70cd853d47c6344ed4cda050
chusamasadiq <usamasadiq163@gmail.com>
2022-11-01 22:14:21 +0500

    Development

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,22 @@
+$(window).on('scroll load', function () {
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
 
+    if ($(window).scrollTop() > 600) {
+        $('.top').show();
+    } else {
+        $('.top').hide();
+    }
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
+});
 
-        if ($(window).scrollTop() > 600) {
-            $('.top').show();
-        } else {
-            $('.top').hide();
-        }
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top,
+    },
+        500,
+        'linear'
+    );
 
-    });
-
-    $('a[href*="#"]').on('click',function(e){
-    
-        e.preventDefault();
-    
-        $('html, body').animate({
-    
-          scrollTop : $($(this).attr('href')).offset().top,
-    
-        },
-          500, 
-          'linear'
-        );
-    
-      });
+});
