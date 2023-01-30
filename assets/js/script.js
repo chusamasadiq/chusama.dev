bd5b0e6ebcf8f5e913f7e9d61726171bbebaa09b
chusamasadiq <usamasadiq163@gmail.com>
2023-01-30 23:06:19 +0500

    usama sadiq

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,32 @@ $('a[href*="#"]').on('click', function (e) {
     );
 
 });
+
+let color = ['red', 'green', 'brown', 'blue']
+function square() {
+    let section = document.querySelector('.banner');
+
+    let square = document.createElement('span');
+
+    let size = Math.random() * 30;
+
+    square.style.width = 20 + size + 'px';
+
+    square.style.height = 20 + size + 'px';
+
+    square.style.top = Math.random() * innerHeight + 'px';
+
+    square.style.left = Math.random() * innerWidth + 'px';
+
+    let bg = color[Math.floor(Math.random() * color.length)];
+
+    square.style.background = bg;
+
+    section.appendChild(square);
+
+    setTimeout(() => {
+        square.remove();
+    }, 5000)
+}
+setInterval(square, 50);
+
