50e62e1124ba46ecd1b93789233e5a28fb6581a8
chusamasadiq <usamasadiq163@gmail.com>
2023-05-07 21:25:17 +0500

    chusama.dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,42 @@ function square() {
 }
 setInterval(square, 50);
 
+const navbarPicture = document.querySelector('#navbar-picture');
+const offcanvasPicture = document.querySelector('#offcanvas-picture');
+const darkModeButton = document.querySelector('#theme-toggle');
+const offcanvasDarkModeButton = document.querySelector('#offcanvas-theme-toggle');
+const lightIcon = darkModeButton.querySelector('.light-icon');
+const offcanvasLightIcon = offcanvasDarkModeButton.querySelector('.light-icon');
+const darkIcon = darkModeButton.querySelector('.dark-icon');
+const offcanvasDarkIcon = offcanvasDarkModeButton.querySelector('.dark-icon');
+const themeText = darkModeButton.querySelector('.theme-text');
+const offcanvasThemeText = offcanvasDarkModeButton.querySelector('.theme-text');
+
+darkModeButton.addEventListener('click', toggleTheme);
+offcanvasDarkModeButton.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+
+    if (document.body.classList.contains('dark-theme')) {
+        lightIcon.style.display = 'none';
+        offcanvasLightIcon.style.display = 'none';
+        darkIcon.style.display = 'inline-block';
+        offcanvasDarkIcon.style.display = 'inline-block';
+        themeText.textContent = 'Dark';
+        offcanvasThemeText.textContent = 'Dark';
+        navbarPicture.src = './assets/images/dark-logo.svg';
+        offcanvasPicture.src = './assets/images/dark-logo.svg';
+    } else {
+        lightIcon.style.display = 'inline-block';
+        offcanvasLightIcon.style.display = 'inline-block';
+        darkIcon.style.display = 'none';
+        offcanvasDarkIcon.style.display = 'none';
+        themeText.textContent = 'Light';
+        offcanvasThemeText.textContent = 'Light';
+        navbarPicture.src = './assets/images/light-logo.svg';
+        offcanvasPicture.src = './assets/images/light-logo.svg';
+    }
+}
+
+
